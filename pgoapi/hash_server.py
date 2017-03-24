defaab6a07dd9b5fa6cab3ac42e695a8d1537f40
voxx <cd@goldencomm.com>
2017-03-23 22:54:53 -0700

    Use http endpoint temporarily due to ssl errors.

--- pgoapi/hash_server.py
+++ pgoapi/hash_server.py
@@ -15,7 +15,7 @@ class HashServer(HashEngine):
     _session.mount('https://', _adapter)
     _session.verify = True
     _session.headers.update({'User-Agent': 'Python pgoapi @pogodev'})
-    endpoint = "https://pokehash.buddyauth.com/api/v127_4/hash"
+    endpoint = "http://pokehash.buddyauth.com/api/v127_4/hash"
     status = {}
 
     def __init__(self, auth_token):
